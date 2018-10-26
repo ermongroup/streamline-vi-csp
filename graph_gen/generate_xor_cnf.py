3cbc892bac5f3d9d122e6fc3cd33b581db1245f0
Tudor Achim <t.achim@gmail.com>
2018-10-26 09:02:24 -0700

    xors: fix the random instance creation

--- graph_gen/generate_xor_cnf.py
+++ graph_gen/generate_xor_cnf.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 assert args.cnf_file_path.endswith('.cnf'), ('Filename must end in .cnf', args.cnf_file_path)
 assert not os.path.exists(args.cnf_file_path), ('File already exists:', args.cnf_file_path)
 
-from sympy.logic.boolalg import ITE, And, Xor, Or
+from sympy.logic.boolalg import ITE, And, Xor, Or, Not
 import sympy
 from sympy.logic.boolalg import to_cnf
 
@@ -29,7 +29,13 @@ variables = sympy.symbols(','.join(map(str, range(1, args.num_variables+1))))
 xors = []
 for _ in range(num_xors):
     xor_vars = random.sample(variables, args.xor_num_vars)
-    xors.append(Xor(*xor_vars))
+    for i in range(len(xor_vars)):
+        if np.random.random() < 0.5:
+            xor_vars[i] = Not(xor_vars[i])
+    xor = Xor(*xor_vars)
+    if np.random.random() < 0.5:
+        xor = Not(xor)
+    xors.append(xor)
 formula = And(*xors)
 cnf_formula = to_cnf(formula)
 
